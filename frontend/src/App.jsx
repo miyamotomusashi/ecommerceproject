d03b3d6a091d050297d6b436070e93d92c683fbe
miyamotomusashi <sa.ordekci@gmail.com>
2024-05-16 16:13:56 +0300

    title icin ek css eklendi

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 //import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-import './App.css'
+
 import { Categories } from './components/Layout/Categories/Categories'
 import Footer from './components/Layout/Footer/Footer'
 import Header from './components/Layout/Header/Header'
 import { Policy } from './components/Layout/Policy/Policy'
 import Product from './components/Layout/Products/Product'
 import { Slider } from './components/Layout/Slider/Slider'
+import './App.css'
 function App() {
     return (
     <>
