273a8ae6b2055c251deb0ad0aeb7d345157c89f6
miyamotomusashi <sa.ordekci@gmail.com>
2024-08-19 18:27:00 +0300

    global basket added to localstorage

--- frontend/src/context/CardProvider.jsx
+++ frontend/src/context/CardProvider.jsx
@@ -1,15 +1,22 @@
-import { createContext } from 'react';
+import { createContext, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 
 export const CardContext = createContext();
 
 const CardProvider = ({ children }) => {
-  const [cartItems, setCardItems] = useState([]);
-  console.log("card items:"+cartItems);
+  const [cartItems, setCardItems] = useState(
+    localStorage.getItem("cardItems") ? JSON.parse(localStorage.getItem("cardItems")) : []
+
+  );
+  useEffect(() => {
+
+    localStorage.setItem("carItems", JSON.stringify(cartItems));
+
+  }, [cartItems]);
   const addToCard = (productItem) => {
     setCardItems((prevItems) => [...prevItems, productItem]);
-}
+  }
   return (
     <CardContext.Provider
       value={{
